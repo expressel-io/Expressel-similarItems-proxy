54d47fba2820118e97581f26d01a849e6416d25b
root <you@example.com>
2018-08-01 23:09:27 -0700

    fixed proxy-server issues

--- server.js
+++ server.js
@@ -4,21 +4,18 @@ const path = require('path');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
-const redirectedPort = 3004;
+// const redirectedPort = 3004;
 
 app.use(morgan('dev'));
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, './public')));
 app.use('/item/:id', express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+console.log('hi')
 
 //TODO: Redirect 4000 to 3003
-const handleRedirect = function(req, res) {
-  const targetUrl = targetBaseUrl + req.originalUrl;
-  res.redirect(targetUrl);
-}
-
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`)
 });
+
